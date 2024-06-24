5429f9d69eb199ca458deed5ced8b73b56377db0
Michel Naoki <michel.naoki@gmail.com>
2024-06-24 00:33:10 -0300

    add images bombonire

--- js/dados.js
+++ js/dados.js
@@ -88,87 +88,87 @@ var MENU = {
     "burgers": [
         {
             "id": "the-gramercy-tavern-burger-4-pack",
-            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
-            "name": "Gramercy Tavern",
+            "img": "./img/cardapio/burguers/snickers.jpg",
+            "name": "Chocolate Snickers",
             "dsc": "The Gramercy Tavern Burger - 4 Pack",
-            "price": 99
+            "price": 3.5
         },
         {
             "id": "shake-shack-shackburger-8-pack",
-            "img": "./img/cardapio/burguers/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.jpg",
-            "name": "Shake Shack",
+            "img": "./img/cardapio/burguers/sonhodevalsa.jpg",
+            "name": "Chocolate Sonho de Valsa",
             "dsc": "Shake Shack ShackBurger® - 8 Pack",
-            "price": 49
+            "price": 2
         },
         {
             "id": "gotts-cheeseburger-kit-for-4",
-            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg",
-            "name": "Gott's Roadside",
+            "img": "./img/cardapio/burguers/batongarotochoc.jpg",
+            "name": "Chocolate Baton Garoto",
             "dsc": "Gott's Complete Cheeseburger Kit for 4",
-            "price": 99
+            "price": 2
         },
         {
             "id": "le-big-matt-kit-for-6",
-            "img": "./img/cardapio/burguers/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg",
-            "name": "Emmy Squared",
+            "img": "./img/cardapio/burguers/pastilagaroto.jpg",
+            "name": "Pastilha Garoto Hortelã",
             "dsc": "Le Big Matt Burger Kit for 6",
-            "price": 99
+            "price": 2
         },
         {
             "id": "shake-shack-shackburger-16-pack",
-            "img": "./img/cardapio/burguers/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.jpg",
-            "name": "Shake Shack",
+            "img": "./img/cardapio/burguers/bisxtra.jpg",
+            "name": "Chocolate Bis Lacta 45g",
             "dsc": "Shake Shack Shackburger® - 16 Pack",
-            "price": 89
+            "price": 3.5
         },
         {
             "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
-            "img": "./img/cardapio/burguers/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.jpg",
-            "name": "Peter Luger Steak House",
+            "img": "./img/cardapio/burguers/hallsmorango.jpg",
+            "name": "Halls Morango",
             "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
-            "price": 175.95
+            "price": 3
         },
         {
             "id": "double-stack-burger-kit-for-4",
-            "img": "./img/cardapio/burguers/handf-double-stack-burger-kit-for-4.4ee9f54b1d6087e9996335f07c13e5cd.jpg",
-            "name": "Holeman & Finch",
+            "img": "./img/cardapio/burguers/mentospure.jpg",
+            "name": "Mentos Pure Fresh",
             "dsc": "Double Stack Burger Kit for 4",
-            "price": 79
+            "price": 3
         },
         {
             "id": "goldbelly-burger-bash-pack",
-            "img": "./img/cardapio/burguers/the-burger-bash-package.bd9d12d031865940bbe5faf15f1a62f8.jpg",
-            "name": "Pat LaFrieda Meats",
+            "img": "./img/cardapio/burguers/mentosfrutas.jpg",
+            "name": "Mentos Frutas Vermelhas",
             "dsc": "Goldbelly \"Burger Bash\" Pack",
-            "price": 109
+            "price": 3
         },
         {
             "id": "burger-au-poivre-kit-4-pack",
-            "img": "./img/cardapio/burguers/burger-au-poivre-kit-4-pack.3ca0e39b02db753304cd185638dad518.jpg",
-            "name": "Raoul's",
+            "img": "./img/cardapio/burguers/kitkat.jpg",
+            "name": "Chocolate KitKat",
             "dsc": "Burger Au Poivre Kit - 4 Pack",
-            "price": 99
+            "price": 3.5
         },
         {
             "id": "goldbelly-burger-blend-4-lbs",
-            "img": "./img/cardapio/burguers/goldbelly-burger-blend-1-lb.13a21b66edf7173a59c75c3a6d2f981b.jpg",
-            "name": "Flannery Beef",
+            "img": "./img/cardapio/burguers/tridenttutti.jpg",
+            "name": "Trident Tutti Frutti",
             "dsc": "Goldbelly Burger Blend - 4 lbs.",
-            "price": 79
+            "price": 3
         },
         {
             "id": "gotts-complete-cheeseburger-kit-for-8",
-            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-8.092aa049d00286fa1733d720decc782e.jpg",
-            "name": "Gott's Roadside",
+            "img": "./img/cardapio/burguers/gomafrutas.jpg",
+            "name": "Bala de Goma Frutas Sortidas",
             "dsc": "Gott's Complete Cheeseburger Kit for 8",
-            "price": 149
+            "price": 2
         },
         {
             "id": "gramercy-tavern-burger-kielbasa-combo",
-            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-47.jpg",
-            "name": "Gramercy Tavern",
+            "img": "./img/cardapio/burguers/prestigio.jpg",
+            "name": "Chocolate Prestigio",
             "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
-            "price": 149
+            "price": 3
         }
     ],
     "sobremesas": [
